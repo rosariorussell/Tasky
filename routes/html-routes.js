c0a2086288065bd2cc06c93dd75928f79acaedba
Russell Rosario <rosariorussell@gmail.com>
2018-05-14 04:02:44 -0400

    added static handlebars pages

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,5 +2,16 @@ module.exports = function(app) {
   app.get('/', (req, res) => {
     res.render("index")
   })
-}
 
+  app.get('/register', (req, res) => {
+    res.render("register")
+  })
+
+  app.get('/login', (req, res) => {
+    res.render("login")
+  })
+
+  app.get('/mytasks', (req, res) => {
+    res.render("tasks")
+  })
+}
