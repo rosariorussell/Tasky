098e371b054df04a1787e27596ca8a0cf52aa89b
Russell Rosario <rosariorussell@gmail.com>
2018-05-17 20:40:51 -0400

    Updated CORS for authorization

--- routes/controller.js
+++ routes/controller.js
@@ -19,7 +19,7 @@ var corsOptions = {
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', "*")
   res.header('Access-Control-Allow-Methods', "GET, PUT, POST, DELETE, PATCH, OPTIONS")
-  res.header('Access-Control-Allow-Headers', "Content-Type")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
   next()
 })
 
