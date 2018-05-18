90aa3b008eedad6b5040c299b713d29b23283e23
Russell Rosario <rosariorussell@gmail.com>
2018-05-17 20:35:43 -0400

    Updated CORS again

--- routes/controller.js
+++ routes/controller.js
@@ -18,7 +18,7 @@ var corsOptions = {
 }
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', "*")
-  res.header('Access-Control-Allow-Methods', "GET, PUT, POST, DELETE, PATCH")
+  res.header('Access-Control-Allow-Methods', "GET, PUT, POST, DELETE, PATCH, OPTIONS")
   res.header('Access-Control-Allow-Headers', "Content-Type")
   next()
 })
